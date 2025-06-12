1689e6e90c4f0d18480aa32c91d579d63374e477
Jedy <jedyviageiro16@gmail.com>
2025-06-12 14:12:00 +0200

    Listar reservas feitas pelo cliente

--- server/routes/reservasRoutes.js
+++ server/routes/reservasRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { criarPreReserva } = require('../controllers/reservasController');
+const { criarPreReserva, listarReservasCliente, cancelarReserva } = require('../controllers/reservasController');
 const { autenticarToken, isCliente } = require('../middlewares/authMiddleware');
 
 router.post('/reservas', autenticarToken, isCliente, criarPreReserva);
+router.get('/reservas', autenticarToken, isCliente, listarReservasCliente);
+router.delete('/reservas/:reserva_id', autenticarToken, isCliente, cancelarReserva);
 
 module.exports = router;
